6d8dae687d2d20bb14323c8cd4cd306577b68d3f
Nicholas Beerbower <nbeerbower@gmail.com>
2023-11-02 09:52:23 -0400

    initial project

---
+++ discord.js
@@ -0,0 +1,32 @@
+const EventEmitter = require('events');
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+
+class Discord extends EventEmitter {
+	constructor(config, token) {
+		super();
+		this.token = token;
+		this.intents = [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent];
+		this.client = new Client({ intents: this.intents });
+
+		this.client.once(Events.ClientReady, c => {
+			console.log(`Discord Ready! Logged in as ${c.user.tag}`);
+		});
+
+		this.client.on(Events.MessageCreate, message => {
+			this.emit('message', message);
+		});
+
+		this.connect();
+	}
+
+	connect() {
+		this.client.login(this.token);
+	}
+
+	send(message) {
+		//const channel = this.client.channels.cache.get(this.channel);
+		//channel.send(message);
+	}
+}
+
+module.exports = Discord;
---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const fs = require('fs');
+const YAML = require('yaml');
+
+const config = YAML.parse(fs.readFileSync('./config.yaml', 'utf8'));
+
+const Discord = require('./discord');
+const discordClient = new Discord(config, process.env.DISCORD_TOKEN);
+
+discordClient.on('message', message => {
+	//console.log('Got message', message.content);
+});
